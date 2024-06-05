bf78433795b8c9643a2d031aeb1e3c110f8968cd
Nas <n.choudhury1@live.co.uk>
2024-06-05 23:48:23 +0100

    first commit

---
+++ blog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-b3ig!c@w*sqd+2e*@3_fcux39q!one+z!+#^3sa@gqmc$=9kh3"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
